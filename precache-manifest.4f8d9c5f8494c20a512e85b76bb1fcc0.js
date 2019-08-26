fe8c31e48f5f39028f34a6148805eeba1378cd4d
Adrien Lepoutre <thewebsdoor@gmail.com>
2019-08-26 10:30:06 -0400

    Updates

---
+++ precache-manifest.4f8d9c5f8494c20a512e85b76bb1fcc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6e095a7cefca75604e3f8adf1c2f4d",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "dede86b503d04eaa49ae",
+    "url": "/Minesweeper/static/css/main.26f0a411.chunk.css"
+  },
+  {
+    "revision": "1323f257b13a8240402b",
+    "url": "/Minesweeper/static/js/2.7ad9e6e0.chunk.js"
+  },
+  {
+    "revision": "dede86b503d04eaa49ae",
+    "url": "/Minesweeper/static/js/main.a30610c6.chunk.js"
+  },
+  {
+    "revision": "aab4fa59d799cff854f7",
+    "url": "/Minesweeper/static/js/runtime~main.7d22eceb.js"
+  }
+]);
